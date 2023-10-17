bbed94f1f18adbedc9b616150b88aafb41561a19
qiyuanchen <chenqiyuan1012@foxmail.com>
2023-10-17 15:13:36 +0800

    update client code for upload page and navigation page

--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -6,13 +6,21 @@
 
         <el-main class="main-content">
             <img src="@/assets/BigData.png" alt="Descriptive text" class="your-image-class" />
-            <div class="button-group">
-                <el-button class="app-button" type="primary" @click="goToVisualPage">
-                    可视化分析
-                </el-button>
-                <el-button class="app-button" type="success" @click="goToAnalysisPage">
-                    统计分析
-                </el-button>
+            <div>
+                <p class="btn-description">点击下方的按钮来上传您的文件。确保文件格式正确，以便于我们为您处理和分析。</p>
+            </div>
+            <div>
+                <el-upload ref="upload" action="http://localhost:5000/upload"
+                    accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+                    :on-success="handleSuccess" :on-error="handleError" :before-upload="beforeUpload">
+                    <el-button slot="trigger" type="primary" class="upload-btn">点击上传</el-button>
+                </el-upload>
+
+                <!-- 数据表格展示区域 -->
+                <el-table :data="tableData" style="width: 100%" v-if="tableData.length">
+                    <el-table-column v-for="(item, index) in columns" :key="index" :prop="item"
+                        :label="item"></el-table-column>
+                </el-table>
             </div>
         </el-main>
 
@@ -25,15 +33,65 @@
 
 
 <script>
+import { ref } from "vue";
+
 export default {
-    methods: {
-        goToVisualPage() {
-            this.$router.push('/visualization');
-        },
-        goToAnalysisPage() {
-            this.$router.push('/analysis');
-        }
-    }
+    name: "UploadComponent",
+    setup() {
+        const tableData = ref([]);
+        const columns = ref([]);
+
+        const handleSuccess = (response) => {
+            let parsedData;
+
+            if (typeof response === "string") {
+                try {
+                    parsedData = JSON.parse(response);
+                } catch (error) {
+                    console.error("Error parsing server response:", error);
+                    return;
+                }
+            } else {
+                parsedData = response;
+            }
+
+            if (parsedData && typeof parsedData.data === "object" && Array.isArray(parsedData.data)) {
+                tableData.value = parsedData.data;
+                if (parsedData.data.length > 0) {
+                    columns.value = Object.keys(parsedData.data[0]);
+                }
+            } else {
+                console.error("Unexpected server response format:", parsedData);
+            }
+        };
+
+        const handleError = (err) => {
+            console.error("Upload failed:", err);
+        };
+
+        const beforeUpload = (file) => {
+            const isExcel = file.type === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" || file.type === "application/vnd.ms-excel";
+            const isCsv = file.type === "text/csv";
+            if (!isExcel && !isCsv) {
+                this.$message.error('上传文件只能是 Excel 或 CSV 格式!');
+                return false;
+            }
+            const isLt2M = file.size / 1024 / 1024 < 2;
+            if (!isLt2M) {
+                this.$message.error('上传文件大小不能超过 2MB!');
+                return false;
+            }
+            return true;
+        };
+
+        return {
+            tableData,
+            columns,
+            handleSuccess,
+            handleError,
+            beforeUpload,
+        };
+    },
 };
 </script>
 
@@ -60,21 +118,17 @@ export default {
     min-height: calc(100vh - 140px);
 }
 
-.button-group {
-    display: flex;
-    flex-direction: row;
-    /* arrange buttons horizontally */
-    align-items: center;
-    justify-content: center;
-    gap: 15px;
-    /* provides space between buttons */
+.upload-btn {
+    background-color: #3498db;
+    color: #ffffff;
+    border-radius: 5px;
+    padding: 20px 30px;
+    font-size: 30px;
+    transition: background-color 0.3s;
 }
 
-.app-button {
-    /* other styles remain unchanged */
-    flex: 1;
-    max-width: 200px;
-    text-align: center;
+.btn-description {
+    font-size: 20px;
 }
 
 .your-image-class {
